d8c8e201429957ca6d7c8a3a17848b437260c0c3
Bubbe65 <filip06eriksson@gmail.com>
2024-03-21 14:34:49 +0100

    zsasd
    asdasda

--- Gacha gameeee/Assets/Skripts/FatBase.cs
+++ Gacha gameeee/Assets/Skripts/FatBase.cs
@@ -2,17 +2,28 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+public class FatBase : MonoBehaviour
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+{
+    [SerializeField]string name;
+
+    [TextArea]
+    [SerializeField] string description;
+
+    [SerializeField] Sprite frontSprite;
+    [SerializeField] Sprite backSprite;
+
+}
+
+public enum FatType
+{
+    Normal,
+    Fire,
+    Water,
+    Ground,
+    Electric,
+    Grass,
+    Light,
+    Darkness,
+    
 }
