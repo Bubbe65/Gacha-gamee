2ddf98eb7bac1dfc2405a4257802e6b612217c07
Bubbe65 <filip06eriksson@gmail.com>
2024-03-21 15:10:50 +0100

    ad
    adad

--- Gacha gameeee/Assets/Item databas scripts/FatBase.cs
+++ Gacha gameeee/Assets/Item databas scripts/FatBase.cs
@@ -2,10 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
+[CreateAssetMenu(fileName = "FatData", menuName = "Fat/Create new fat")]
 public class FatBase : MonoBehaviour
 
 {
-    [SerializeField]string name;
+    [SerializeField]string Name;
 
     [TextArea]
     [SerializeField] string description;
@@ -13,6 +15,15 @@ public class FatBase : MonoBehaviour
     [SerializeField] Sprite frontSprite;
     [SerializeField] Sprite backSprite;
 
+    [SerializeField] FatBase type1;
+    [SerializeField] FatBase type2;
+
+    [SerializeField] int maxHp;
+    [SerializeField] int attack;
+    [SerializeField] int defense;
+    [SerializeField] int spAttack;
+    [SerializeField] int spdefense;
+    [SerializeField] int speed;
 }
 
 public enum FatType
