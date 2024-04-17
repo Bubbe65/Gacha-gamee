299267825caaf27f9ff5b16a587b8c50faaae4ee
fillitus <157714370+fillitus@users.noreply.github.com>
2024-04-17 09:17:24 +0200

    lol

--- Gacha gameeee/Assets/Skripts/Battlesystem.cs
+++ Gacha gameeee/Assets/Skripts/Battlesystem.cs
@@ -2,17 +2,25 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum BattleState { START, PLAYERTURN, ENEMYTURN, WON, LOST }
+
 public class Battlesystem : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public GameObject playerPrefab;
+    public GameObject enemyPrefab;
+
+
+
+    public BattleState state;
     void Start()
     {
-        
+        state = BattleState.START;
+        SetupBattle();
     }
 
-    // Update is called once per frame
-    void Update()
+    void SetupBattle()
     {
-        
+
     }
+    
 }
