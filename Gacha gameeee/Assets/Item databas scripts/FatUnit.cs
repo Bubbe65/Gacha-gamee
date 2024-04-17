fd1fd82635c7ecbd2fd44cd502220e92d95fb2ae
Bubbe65 <filip06eriksson@gmail.com>
2024-04-17 08:53:56 +0200

    awd
    adad

---
+++ Gacha gameeee/Assets/Item databas scripts/FatUnit.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FatUnit : MonoBehaviour
+{
+    public string FatName;
+    public int FatLevel;
+
+    public int damage;
+
+    public int maxHp;
+    public int currentHp;
+}
