50deda4427d091fa7811ca9d565d3622d1efc9b7
Azputz <46539491+Azputz@users.noreply.github.com>
2024-04-25 15:14:58 +0200

    sss

---
+++ Gacha gameeee/Assets/Skripts/testmovment.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Testingmovement : MonoBehaviour
+{
+    public float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontalinput = Input.GetAxis("Horizontal");
+        float verticalinput = Input.GetAxis("Vertical");
+
+        Vector3 direction = new(horizontalinput, verticalinput, 0);
+        transform.Translate(direction * speed * Time.deltaTime);
+
+    }
+}
