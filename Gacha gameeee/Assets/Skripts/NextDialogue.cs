0c88385766fe133919b3961b1341d082ee0a17d9
Azputz <46539491+Azputz@users.noreply.github.com>
2024-06-12 09:27:21 +0200

    jjj

--- Gacha gameeee/Assets/Skripts/NextDialogue.cs
+++ Gacha gameeee/Assets/Skripts/NextDialogue.cs
@@ -7,6 +7,7 @@ public class NextDialogue : MonoBehaviour
     int index = 2;
     private void Update()
     {
+        //Denna rad av kod gör att när man klickar på musen så går man över till nästa dialogruta
         if(Input.GetMouseButtonDown(0) && transform.childCount > 1)
         {
             if(Testingmovement.dialogue)
